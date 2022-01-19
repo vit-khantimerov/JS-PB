976bb44769931786aa867571338546573c8a218c
vit-khantimerov <92208142+vit-khantimerov@users.noreply.github.com>
2022-01-19 09:36:27 +0200

    Task 9

--- HW 2/task 2 - last.js
+++ HW 2/task 2 - last.js
@@ -1,32 +1,33 @@
-/* ДЗ 2. Задача *.
+/* ДЗ 2. Задача 9.
 
-Сложение в столбик.
+function solution(a, b) {
+    //....
+    return [];
+}
 
+
+
+console.log(solution([2, 4, 3], [5, 6, 4])); // [8, 0, 7]
+console.log(solution([1, 4, 5], [4, 4, 2])); // [5, 8, 7]
+console.log(solution([1, 1, 1], [])); // [1, 1, 1]
+console.log(solution([], [9, 9, 9])); // [9, 9, 9]
+console.log(solution([9,9,9], [9,9,9])); // [1, 9, 9, 8]
+console.log(solution([], [])); // []
+console.log(solution([], [0])); // [0]
+console.log(solution([0], [0])); // [0]
+console.log(solution([1,1,1,1,1,1,1,1], [1, 0])); // [1, 1, 1, 1, 1, 1, 2, 1]
+console.log(solution([9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[1]));
+// // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 */
 
-// Решение.
-
-// считаем, что массивы состоят только из цифр
-
-var ar1 = [0, 0, 0, 5, 4, 3, 9];
-var ar2 = [0, 0, 0, 8, 7, 6, 9];
-
-var ar3 = [9, 9, 9, 9, 9, 9];
-var ar4 = [1, 0, 0, 1];
-
-var ar5 = [9, 2, 3, 7];
-var ar6 = [4, 5, 6];
-
-var ar7 = [1, 2, 3];
-var ar8 = [1, 1, 1];
-
-var ar9 = [0, 0, 0, 0, 0];
-var ar10 = [1];
 
 
-function summ (arr1, arr2){
-    console.log('array 1 : ' + arr1);
-    console.log('array 2 : ' + arr2);
+//  Решение
+
+
+function solution (a, b){
+    // console.log('array 1 : ' + a);
+    // console.log('array 2 : ' + b);
 
 
 // Определяем длинный массив как базу для итога.
@@ -35,14 +36,14 @@ function summ (arr1, arr2){
     var short = new Array;
 
 
-    if (arr1.length >= arr2.length) {
-        length = arr1.length;
-        long = arr1.reverse(); // реверс массива для удобства итерраций
-        short = arr2.reverse();
+    if (a.length >= b.length) {
+        length = a.length;
+        long = a.reverse(); // реверс массива для удобства итерраций
+        short = b.reverse();
     } else {
-        length = arr2.length;
-        long = arr2.reverse();
-        short = arr1.reverse();
+        length = b.length;
+        long = b.reverse();
+        short = a.reverse();
     }
 
     var ten = 0; // =1 при сумме >= 10 (переходящая единица)
@@ -74,30 +75,76 @@ function summ (arr1, arr2){
     var itog = new Array;
     itog = long.reverse();
 
-    console.log('result  : ' + itog + '\n');
+    // console.log('result  : ' + itog + '\n');
+    return itog;
 }
 
-summ(ar1, ar2);
+console.log(solution([2, 4, 3], [5, 6, 4])); // [8, 0, 7]
+console.log(solution([1, 4, 5], [4, 4, 2])); // [5, 8, 7]
+console.log(solution([1, 1, 1], [])); // [1, 1, 1]
+console.log(solution([], [9, 9, 9])); // [9, 9, 9]
+console.log(solution([9,9,9], [9,9,9])); // [1, 9, 9, 8]
+console.log(solution([], [])); // []
+console.log(solution([], [0])); // [0]
+console.log(solution([0], [0])); // [0]
+console.log(solution([1,1,1,1,1,1,1,1], [1, 0])); // [1, 1, 1, 1, 1, 1, 2, 1]
+console.log(solution([9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[1]));
+// // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+// Мой результат:
+// [ 8, 0, 7 ]
+// [ 5, 8, 7 ]
+// [ 1, 1, 1 ]
+// [ 9, 9, 9 ]
+// [ 1, 9, 9, 8 ]
+// []
+// [ 0 ]
+// [ 0 ]
+// [ 1, 1, 1, 1, 1, 1, 2, 1 ]
+// [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+
+
+/////// Еще несколько тестовых примеров.
+
+// var ar1 = [0, 0, 0, 5, 4, 3, 9];
+// var ar2 = [0, 0, 0, 8, 7, 6, 9];
+
+// var ar3 = [9, 9, 9, 9, 9, 9];
+// var ar4 = [1, 0, 0, 1];
+
+// var ar5 = [9, 2, 3, 7];
+// var ar6 = [4, 5, 6];
+
+// var ar7 = [1, 2, 3];
+// var ar8 = [1, 1, 1];
+
+// var ar9 = [0, 0, 0, 0, 0];
+// var ar10 = [1];
+
+
+// console.log(solution(ar1, ar2));
 // array 1 : 0,0,0,5,4,3,9
 // array 2 : 0,0,0,8,7,6,9
 // result  : 0,0,1,4,2,0,8
 
-summ(ar3, ar4);
+// console.log(solution(ar3, ar4));
 // array 1 :   9,9,9,9,9,9
 // array 2 :       1,0,0,1
 // result  : 1,0,0,1,0,0,0
 
-summ(ar5, ar6);
+// console.log(solution(ar5, ar6));
 // array 1 : 9,2,3,7
 // array 2 :   4,5,6
 // result  : 9,6,9,3
 
-summ(ar7, ar8);
+// console.log(solution(ar7, ar8));
 // array 1 : 1,2,3
 // array 2 : 1,1,1
 // result  : 2,3,4
 
-summ(ar9, ar10);
+// console.log(solution(ar9, ar10));
 // array 1 : 0,0,0,0,0
 // array 2 :         1
 // result  : 0,0,0,0,1
