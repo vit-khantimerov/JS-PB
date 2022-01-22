09526754f24d93cfdb3e4a965f457181422dbf5c
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2022-01-23 01:00:34 +0200

    1

---
+++ Test JS/test alert console.js
@@ -0,0 +1,22 @@
+console.clear(); // not works out of terminal
+
+[1, 2].forEach(console.log);
+console.log("██");
+
+let num = 255;
+
+console.log(num.toString(16)); // ff
+console.log(num.toString(2)); // 11111111
+console.log(num.toString()); //10  // 11111111
+console.log(num.toString(35)); // 11111111
+console.log(9999999999999999999n + 9999999999999999999n);
+console.log(100);
+console.log("100");
+console.log(1 / -0);
+console.log(parseInt("100px00"));
+console.log("\t" + "abcde" + "\b, \f, \v" + "aaaaaa"); //         abcd, ♀, ♂aaaaaa
+console.log(`1a
+2b
+3c`);
+console.log(`summ = ${num + num}\nmulti = ${num * num}`);
+console.log(console.log());
