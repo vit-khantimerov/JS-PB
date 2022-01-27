0c3666d49e81a8e3b158e66e7a1d9e1de990e87e
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2022-01-27 23:28:58 +0200

    L 4

--- Test JS/lambda.js
+++ Test JS/lambda.js
@@ -1,22 +1,33 @@
+let ff = () => "Hello Lambda!";
+console.log(ff()); // Hello Lambda!
+
 /************************/
 let sayHi = () => console.log("Hello lambda!");
-sayHi();           // Hello lambda!
+sayHi(); // Hello lambda!
+
+/************************/
+sayHi = (s, t) => console.log(s, t);
+sayHi("Hello", "lambda!"); // Hello lambda!
+
+sayHi(); // undefined undefined
+sayHi(123); // 123 undefined
+sayHi("1", "+ 2 = 3"); // 1 + 2 = 3
 
 /************************/
 let sum = (a, b) => a + b; //  f(a,b){a+b}
-console.log(sum(100,11)); // 111
+console.log(sum(100, 11)); // 111
 
 /************************/
-let cube = a => a ** 3;
-console.log( cube(3) ); // 27
+let cube = (a) => a ** 3;
+console.log(cube(3)); // 27
 
 /************************/
 let fun = (a, b) => {
-    console.log(a * b);
-    console.log(a / b);
-    console.log(a - b);
-    return a + b;
-}
+  console.log(a * b);
+  console.log(a / b);
+  console.log(a - b);
+  return a + b;
+};
 console.log(fun(100, 7));
 // 700
 // 14.285714285714286
@@ -24,16 +35,12 @@ console.log(fun(100, 7));
 // 107
 
 /************************/
-let age = 10, x = 18;
+let age = 10,
+  x = 18;
 
-let welcome = (age < x) ?
-  () => console.log('Привет') :
-  () => console.log("Здравствуйте!");
+let welcome =
+  age < x ? () => console.log("Привет") : () => console.log("Здравствуйте!");
 
 welcome();
 age = 20;
 welcome();
-
-// he080372kvg@HEA0-797206:~/JS-PB$ node "/home/HE080372KVG/JS-PB/Test JS/tempCodeRunnerFile.js"
-// Привет
-// Привет // хм
