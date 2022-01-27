4e049146e0939d2c7020cb1fa9cfd8fba29814d1
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2022-01-27 21:50:44 +0200

    lesson 4

--- Transfer/Les 4/task 4 - 1 draft.js
+++ Transfer/Les 4/task 4 - 1 draft.js
@@ -14,7 +14,6 @@ forEach(arr, function(item, i, arr) {});
 ```
 */
 
-<<<<<<< HEAD
 // Решение
 
 function forEach(arr, f) {
@@ -139,57 +138,3 @@ Error: Error: parameter 2 type should be a function
 
 [Done] exited with code=0 in 0.35 seconds
 */
-=======
-
-// Решение
-
-function forEach(arr, f) {
-    if (!Array.isArray(arr)) {
-        throw new Error('Error: parameter 1 type should be an array');
-    }
-    if (typeof(f) !== 'function') {
-        throw new Error('Error: parameter 2 type should be a function');
-    }
-    for (let i = 0; i < arr.length; i++) {
-        // console.log(i);
-        
-        f(arr[i], i, arr);     
-    }
-}
-
-function clg(item, i, arr) {
-    console.log(`item = ${item}, i = ${i}, arr = ${arr}`);
-}
-
-function f2(item, i, arr) {
-    console.log(item * 2);
-}
-
-function f3(item, i, arr) {
-    console.log(item, typeof(item));
-}
-
-let aaa = [1,2,3,4,5];
-forEach(aaa, clg);
-
-console.log(forEach(aaa, f2));
-console.log(forEach([1,2,3], f3));
-
-forEach([1, 2, 3, 's'], function f4(item, i, arr) {
-    console.log(item * 10, i * 10, arr);
-});
-
-
-aaa.forEach(f2);
-
-forEach([0], clg);
-console.log(forEach([0], f3));
-
-console.log(forEach([0], 0));
-
-
-
-// Результат
-
-// 
->>>>>>> 4aba8e3ed3ad97ce20e979adf35cfab645058b12
